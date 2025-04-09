82975f942f658514cba4e54be8845f6447cda357
Kriti Bharadwaj <93143184+gypsybozo@users.noreply.github.com>
2025-04-09 23:39:08 +0530

    Before deploying

--- src/main/java/com/placementtracker/PlacementTrackerApplication.java
+++ src/main/java/com/placementtracker/PlacementTrackerApplication.java
@@ -3,12 +3,17 @@ package com.placementtracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableScheduling
 public class PlacementTrackerApplication {
-
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(PlacementTrackerApplication.class, args);
     }
 }
